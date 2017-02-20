16b2de3c934f8ccc9a307d796b550c25e54d55c1
Rene <Rene@null.spigotmc.org>
2017-02-20 10:04:28 +0100

    Updated ClassPath

--- TinkerTools/src/net/jartex/tinkertools/TinkerTools.java
+++ TinkerTools/src/net/jartex/tinkertools/TinkerTools.java
@@ -1,5 +1,22 @@
 package net.jartex.tinkertools;
 
-public class TinkerTools {
+import java.util.logging.Level;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TinkerTools extends JavaPlugin{
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	void log(String s) {
+		String s1 = "TinkerTools: " + s;
+		getLogger().log(Level.INFO, s1);
+	}
 
 }
