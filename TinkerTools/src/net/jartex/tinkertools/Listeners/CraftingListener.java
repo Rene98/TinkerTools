1073a35ecb5061dddd7d181b6f62bbfeb5b810b5
Rene <Rene@null.spigotmc.org>
2017-02-20 15:06:00 +0100

    Added tinkertools

--- TinkerTools/src/net/jartex/tinkertools/Listeners/CraftingListener.java
+++ TinkerTools/src/net/jartex/tinkertools/Listeners/CraftingListener.java
@@ -7,9 +7,11 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class CraftingListener implements Listener {
 	ArrayList<Material> bannedTools = new ArrayList<Material>(Arrays.asList(
@@ -18,7 +20,9 @@ public class CraftingListener implements Listener {
 			Material.GOLD_AXE, Material.GOLD_HOE, Material.GOLD_PICKAXE, Material.GOLD_SPADE, Material.GOLD_SWORD,
 			Material.DIAMOND_AXE, Material.DIAMOND_HOE, Material.DIAMOND_PICKAXE, Material.DIAMOND_SPADE, Material.DIAMOND_SWORD
 			));
-	
+	ArrayList<Material> allowedTools = new ArrayList<Material>(Arrays.asList(
+			Material.WOOD_AXE, Material.WOOD_HOE, Material.WOOD_PICKAXE, Material.WOOD_SPADE, Material.WOOD_SWORD
+			));
 	@EventHandler
 	public void onCraft(PrepareItemCraftEvent e) {
 		Material result = e.getRecipe().getResult().getType();
@@ -29,6 +33,10 @@ public class CraftingListener implements Listener {
                     ((Player)he).sendMessage(ChatColor.RED+"You cannot craft this!");
                 }
             }
+		} else if(allowedTools.contains(result)) {
+			ItemStack TinkerTool = new ItemStack(result);
+			ItemMeta tinkermeta = TinkerTool.getItemMeta(); 
+			tinkermeta.setLore(Arrays.asList(ChatColor.GREEN + "Tinker Tool", ChatColor.GOLD + "BrandNew: (0/250)"));
 		}
 	}
 }
